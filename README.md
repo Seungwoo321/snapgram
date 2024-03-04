9619cd0a41089ddb0bf1886f479167a483985005
Seungwoo321 <seungwoo321@gmail.com>
2024-03-04 17:27:19 +0900

    feat: add first react project snapgram

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# react-project
+# React Project 100
+
+1. Snapgram
+
+- [인스타그램 클론 프로젝트]('./snapgram')
+- 유튜브: <https://www.youtube.com/watch?v=_W3R2VwRyF4&t=678s>
+- 데모:
---
+++ snapgram/src/App.tsx
@@ -0,0 +1,33 @@
+import { Routes, Route } from 'react-router-dom';
+
+import './globals.css';
+import SigninForm from './_auth/formas/SigninForm';
+import SignupForm from './_auth/formas/SignupForm';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Toaster } from "@/components/ui/toaster"
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
+        </Route>
+
+
+        {/* private reoutes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+
+      <Toaster />
+    </main>
+  )
+}
+
+export default App

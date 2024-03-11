7e380200430a82121a8ef3b5904fc74cc4f4bba1
Seungwoo321 <seungwoo321@gmail.com>
2024-03-11 17:15:21 +0900

    feat: add save and like

--- snapgram/src/lib/utils.ts
+++ snapgram/src/lib/utils.ts
@@ -22,3 +22,28 @@ export function formatDateString(dateString: string) {
 
   return `${formattedDate} at ${time}`;
 }
+
+export function formatData(dateString: string): string {
+  const currentDate = new Date();
+  const inputDate = new Date(dateString);
+
+  const timeDifference = currentDate.getTime() - inputDate.getTime();
+  const secondsDifference = timeDifference / 1000;
+
+  if (secondsDifference < 60) {
+    return `${Math.floor(secondsDifference)} seconds ago`;
+  } else if (secondsDifference < 3600) {
+    const minutes = Math.floor(secondsDifference / 60);
+    return `${minutes} ${minutes === 1 ? 'minute' : 'minutes'} ago`;
+  } else if (secondsDifference < 86400) {
+    const hours = Math.floor(secondsDifference / 3600);
+    return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
+  } else {
+    const days = Math.floor(secondsDifference / 86400);
+    return `${days} ${days === 1 ? 'day' : 'days'} ago`;
+  }
+}
+
+export const checkIsLiked = (likeList: string[], userId: string) => {
+  return likeList.includes(userId);
+}
